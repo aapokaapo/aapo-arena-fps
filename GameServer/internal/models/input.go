0dc87ba1e35982351d92846291c4c267c6be108a
Aapo Kinnunen <aapok98@gmail.com>
2026-05-22 20:25:40 +0300

    server fixes

--- GameServer/internal/models/input.go
+++ GameServer/internal/models/input.go
@@ -1,1 +1,18 @@
 package models
+
+// PlayerInput represents a single frame of input from the Godot client
+type PlayerInput struct {
+	CommandID uint64  `json:"cid"`
+	DeltaTime float32 `json:"dt"`
+	MoveX     float32 `json:"mx"`
+	MoveY     float32 `json:"my"`
+	Pitch     float32 `json:"p"`
+	Yaw       float32 `json:"y"`
+	Buttons   uint8   `json:"b"`
+}
+
+// ClientInputPacket is the payload sent over WebTransport datagrams
+type ClientInputPacket struct {
+	LatestTick uint64        `json:"t"`
+	Inputs     []PlayerInput `json:"i"`
+}
